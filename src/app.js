2ce4bb8a2aab0cc82f5772a7af42cd18be865b6d
onkarbirajdaar <onkarbirajdar098@gmail.com>
2026-08-09 17:19:32 +0530

    Switch to prisma-client-js generator, add test-db route

--- src/app.js
+++ src/app.js
@@ -6,4 +6,7 @@ const healthRoutes = require('./routes/health');
 app.use('/health', healthRoutes);
 const movieRoutes = require('./routes/movies');
    app.use('/movies', movieRoutes);
+
+const dbRoutes = require('./routes/test');
+app.use('/test-db', dbRoutes);
 module.exports = app;
---
+++ src/controllers/testController.js
@@ -0,0 +1,16 @@
+const prisma = require('../config/prisma');
+
+const testDb = async (requestAnimationFrame, res) => {
+    try {
+        const city = await prisma.city.create({
+            data: { name: 'Mumbai'},
+        });
+        res.json({message: "Database write successful", city});
+    } catch (error){
+        console.error(error);
+        res.status(500).json({error: 'Database write failed'});
+    }
+
+};
+
+module.exports = { testDb };
